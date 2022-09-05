6eb1d48622bda940337bc21269577902ac5ed396
Lynn W Mwaniki <mwanikilynn8@gmail.com>
2022-09-05 17:04:02 +0300

    edited function getMealList

--- food-recipe-web-IP/index.js
+++ food-recipe-web-IP/index.js
@@ -4,19 +4,20 @@ const mealDetailsContent = document.querySelector('meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn')
 
 // adding event listeners
-searchBtn.addEventListener('click', getMealList)
+searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+
 // get the list of meals that match with the ingredients
 function getMealList(){
     let searchInputText = document.getElementById('search-input').value.trim();
-    //console.log(searchInputText);
-    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputText}')
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputText}`)
     .then(resp => resp.json())
-    .then(data => /*console.log(data)*/{
+    .then(data => {
         let html = "";
         if(data.meals){
             data.meals.forEach(meal =>{
                 html += `
-                <div class = "meal-item" data-id = "${meal.idMeal}">
+                <div class = "meal-item" data-id = ${meal.idMeal}">
                     <div class = "meal-img">
                         <img src = "${meal.strMealThumb}" alt = "food">
                     </div>
@@ -35,3 +36,15 @@ function getMealList(){
         mealList.innerHTML = html;
     });
 }
+
+//getting the recipes of the meals
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+    let mealItem = e.target.parentElement.parentElement;
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+    .then(resp => resp.json())
+    .then(data => mealRecipeModal(data.meals));
+}
+}
+
