aac1c0e5f3debdd0034e4e8d4db19bd03dfbe6d8
Lynn W Mwaniki <mwanikilynn8@gmail.com>
2022-09-06 09:56:31 +0300

    created a modal

--- food-recipe-web-IP/index.js
+++ food-recipe-web-IP/index.js
@@ -48,3 +48,24 @@ function getMealRecipe(e){
 }
 }
 
+// modal creation(A modal window creates a mode that disables the main window but keeps it visible, with the modal window as a child window in front of it.)
+function mealRecipeModal(meal){
+    console.log(meal);
+    meal = meal[0];
+    let html = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+        <div class = "recipe-link">
+            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    `;
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe'); 
+}
